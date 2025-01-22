4bcef9096f82ff852a1a0fb5a776e8791357130a
Suvigya <suvigyasrvstva@gmail.com>
2025-01-22 14:03:14 +0530

    Removed hardcoded keys and updated for deployment

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
+// Create a Supabase client using environment variables
 const supabase = createClient(
-  "https://vuacldbohptgllwdlzuv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1YWNsZGJvaHB0Z2xsd2RsenV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0ODQxODIsImV4cCI6MjA1MzA2MDE4Mn0.CP2Mdw5nBpjD32LQqLg7pcRocraj9pYwANy7PrGh4EQ" 
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,7 +21,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: Log performance metrics
 reportWebVitals();
